7d53f709f3d1f97b66e407ea3bbf3c780b8967cb
dequilla3 <deqkim4@gmail.com>
2023-09-26 17:06:24 +0800

    feat: added name col on user

--- src/model/user.entity.ts
+++ src/model/user.entity.ts
@@ -11,6 +11,12 @@ export class User {
   @Column()
   password: string;
 
+  @Column({ nullable: true })
+  firstname: string;
+
+  @Column({ nullable: true })
+  lastname: string;
+
   @Column({ type: 'timestamptz', nullable: true })
   dateTime: Date;
 
